b3e0f1caa81ef24ffe950450b81e008883c797ab
av_dovnar <dovnar.av@gmail.com>
2021-12-19 13:41:32 +0300

    done

--- tasks/phone.py
+++ tasks/phone.py
@@ -17,3 +17,30 @@
 Модель: {}
 Год выпуска: {}
 """
+
+
+class Phone:
+    brand: str
+    model: str
+    issue_year: int
+
+    def __init__(self, brand, model, issue_year):
+        self.brand = brand
+        self.model = model
+        self.issue_year = issue_year
+
+    def __str__(self):
+        return f"Бренд: {self.brand}\nМодель: {self.model}\nГод выпуска: {self.issue_year}"
+
+    def receive_call(name: str):
+        print(f"Звонит {name}")
+
+    def get_info(self) -> tuple:
+        new_tuple = (self.brand, self.model, self.issue_year)
+        return new_tuple
+
+
+Phone.receive_call("Mam")
+phone = Phone("Apple", "iPhone 6", 2014)
+print(phone.get_info())
+print(phone)
--- tasks/student.py
+++ tasks/student.py
@@ -19,3 +19,52 @@
 
 Вывести студентов, у которых средний балл больше 5
 """
+
+
+class Student:
+    surname: str
+    name: str
+    group: int
+    average_score: float
+
+    def __init__(self, surname, name, group, average_score):
+        self.surname = surname
+        self.name = name
+        self.group = group
+        self.average_score = average_score
+
+    def __gt__(self, other):
+        return self.average_score > other.average_score
+
+    def __le__(self, other):
+        return self.average_score <= other.average_score
+
+    def __ge__(self, other):
+        return self.average_score >= other.average_score
+
+    def __lt__(self, other):
+        return self.average_score < other.average_score
+
+    def __eq__(self, other):
+        return self.average_score == other.average_score
+
+    def __ne__(self, other):
+        return self.average_score != other.average_score
+
+    def __repr__(self):
+        return f"Name = {self.name}, Surname = {self.surname}, "\
+               f"Group = {self.group}, Average_score = {self.average_score}."
+
+
+students_list = [
+    Student('Anton', 'Dovnar', 33, 9.6),
+    Student('Vova', 'Zaicev', 27, 3.2),
+    Student('Eva', 'Petrova', 31, 7.3),
+    Student('Slava', 'Dubok', 33, 2.9),
+    Student('Klava', 'Xodich', 25, 4.5)
+]
+
+print(sorted(students_list))
+print(sorted(students_list, reverse=True))
+best_list = [student for student in students_list if student.average_score > 5]
+print(best_list)
